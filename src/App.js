2a694f8abe698d19402de2629d146c1f64b87bf5
sinbox0701 <psh090953@gmail.com>
2021-09-07 10:55:18 +0900

    React Var

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {useReactiveVar} from "@apollo/client";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import { isLoggedInVar } from "./apollo";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
-    <div >
-      
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home/> : <Login/>}
+          </Route>
+          <Route>
+            <NotFound/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/screens/Login.js
@@ -0,0 +1,12 @@
+import { isLoggedInVar } from "../apollo";
+
+function Login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <button onClick={()=>isLoggedInVar(true)}>Log In!</button>
+        </div>
+    );
+}
+
+export default Login;
