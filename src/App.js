e725b7e53e40790486bb7d43e8d46c2a57b4e971
sinbox0701 <psh090953@gmail.com>
2021-09-07 15:26:37 +0900

    React Hook Form

--- src/App.js
+++ src/App.js
@@ -8,30 +8,33 @@ import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import SignUp from "./screens/SignUp";
 import routes from "./routes";
+import {HelmetProvider} from "react-helmet-async";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
-    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <GlobalStyles/>
-      <Router>
-        <Switch>
-          <Route path={routes.home} exact>
-            {isLoggedIn ? <Home/> : <Login/>}
-          </Route>
-          {!isLoggedIn ? (
-            <Route path={routes.signUp}>
-              <SignUp/>
-            </Route>  
-          ):null}
-          <Route>
-            <NotFound/>
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <GlobalStyles/>
+        <Router>
+          <Switch>
+            <Route path={routes.home} exact>
+              {isLoggedIn ? <Home/> : <Login/>}
+            </Route>
+            {!isLoggedIn ? (
+              <Route path={routes.signUp}>
+                <SignUp/>
+              </Route>  
+            ):null}
+            <Route>
+              <NotFound/>
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </HelmetProvider>
   );
 }
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,15 @@
 import { faFacebookSquare, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
 import BottomBox from "../components/auth/BottomBox";
 import Button from "../components/auth/Button";
 import FormBox from "../components/auth/FormBox";
+import FormError from "../components/auth/FormError";
 import Input from "../components/auth/Input";
 import Separator from "../components/auth/Separator";
+import PageTitle from "../components/PageTitle";
 import routes from "../routes";
 
 const FacebookLogin = styled.div`
@@ -19,16 +21,51 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
+    const {register,handleSubmit,errors,formState} = useForm({
+        mode:"onChange",
+    })
+    //mode --> form의 유효성을 검증하는 많은 모드 존재
+    //mode가 onChange 이므로 Input data 값이 유효하게 입력되었는지 확인 유효하면 true 반환 아니면 false 반환
+    //register --> 태그에 ref 사용 각각 태그 식별을 위해 name 사용
+    //watch --> value 확인
+    //error --> 태그에 만들어논 제한 조건(valid)에 위반하면 실시간으로 에러 데이터 저장 
+    //formState --> data 변경이 일어나면 전부 확인 formState.isValid 유효성 검사 후 맞으면 true
+    const onSubmitValid = (data) => {
+        console.log(data);
+    }
     return (
         <AuthLayout>
+            <PageTitle title="Login" />
             <FormBox>
                 <div>
                     <FontAwesomeIcon icon={faInstagram} size="3x"/>
                 </div>
-                <form>
-                    <Input type="text" placeholder="Username" />
-                    <Input type="password" placeholder="Password" />
-                    <Input type="submit" value="Login" />
+                <form onSubmit={handleSubmit(onSubmitValid)}>
+                    <Input 
+                        ref={register({
+                            required:"Username is required",
+                            minLength:{
+                                value:5,
+                                message:"Username should be longer than 5 chars."
+                            }
+                        })}
+                        name="username"
+                        type="text" 
+                        placeholder="Username"
+                        hasError={Boolean(errors?.username?.message)} 
+                    />
+                    <FormError message={errors?.username?.message} />
+                    <Input 
+                        ref={register({
+                            required:"Password is required",
+                        })}
+                        name="password"
+                        type="password" 
+                        placeholder="Password"
+                        hasError={Boolean(errors?.password?.message)} 
+                    />
+                    <FormError message={errors?.password?.message} />
+                    <Button type="submit" value="Login" disabled={!formState.isValid} />
                 </form>
                 <Separator>
                     <div></div>
