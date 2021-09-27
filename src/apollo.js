244eb5582ed035cb8ab49cf12f38a2ad86c6d417
sinbox0701 <psh090953@gmail.com>
2021-09-27 10:25:32 +0900

    apollo

--- src/apollo.js
+++ src/apollo.js
@@ -28,7 +28,7 @@ export const disableDarkMode = () => {
 
 const httpLink = createHttpLink({
     uri:"http://localhost:4000/graphql",
-});//기존 client의 uri를 받아주는 링크 
+});
 
 const authLink = setContext((_,{headers}) => {
     return {
@@ -37,17 +37,18 @@ const authLink = setContext((_,{headers}) => {
             token: localStorage.getItem(TOKEN),
         },
     };
-});//http header customizing --> token을 가져오게끔
-
+});
 export const client = new ApolloClient({
     link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+        typePolicies:{
+            User:{
+                keyFields: (obj) => `User:${obj.username}`
+            }
+        }
+    }),
 });
-//uri대신 link를 이용해 백엔드 연결
 /*
-    protectedResolver --> token이 유효하지 않거나 없을때 null을 반환
-    localStorage에 token이 존재하지만 backend에서 작동X ==> logOut시키기 ==> token을 가짜로 만드는 것 X --> 보안적 측면
-    localStorage에 token이 존재하지만 backend에서 사용 X ==> Client에게 token을 사용하라는 명령X
-    --> apollo.js 변경 필요
+Apollo Cache 이용 --> 객체를 명확하게 구분하기 위해서 (with 고유식별자)
     
  */
